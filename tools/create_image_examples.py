b240e6bd1bdf3cee5d7d181d75d6aa372bf009d9
Druzhinin <dipetm@gmail.com>
2019-11-15 20:11:46 +0300

    Tool for creating transforms examples

--- tools/create_image_examples.py
+++ tools/create_image_examples.py
@@ -1,6 +1,6 @@
+import os
 import io
 import cv2
-import argparse
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -48,9 +48,9 @@ def apply_transform(
             args_i["p"] = 1
 
         if i == 1:
-            title = "Original"
+            title = f"{i}. Original"
         else:
-            title = ", ".join([f"{name}={val}" for name, val in args_i.items()])
+            title = ", ".join([f"{i}. {name}={val}" for name, val in args_i.items() if name != "p"])
 
         ax = figure.add_subplot(nrows, ncols, i)
         ax.set_title(title)
@@ -87,31 +87,73 @@ def create_docs_text(cls, args):
     return text
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Create text for transform example")
-    parser.add_argument("-a", "--arg", action="append", help="transform argument name")
-    parser.add_argument("-v", "value", action="append", help="transform argument value")
-    parser.add_argument(
-        "-s", "--save", metavar="DIR", default=None, help="path to docs file to write results at end of it is."
+def show_image(image, dpi=200):
+    plt.figure(figsize=(1920 / dpi, 1080 / dpi), dpi=dpi)
+    plt.imshow(image)
+    plt.axis("off")
+    plt.show()
+
+
+def save_results(cls, text, image, save_path):
+    with open(save_path, "a") as file:
+        file.write("\n\n" + text)
+
+    image_path = os.path.split(save_path)[0]
+    image_path = os.path.join(image_path, f"images/{cls.__name__}.jpg")
+    cv2.imwrite(image_path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+
+
+def create_example(
+    transform_cls, args, image_path, targets=None, nrows=None, ncols=None, height=1080, width=1920, dpi=200
+):
+    image = load_rgb_image(image_path)
+
+    args, image = apply_transform(
+        image=image,
+        transform_cls=transform_cls,
+        args=args,
+        targets=targets,
+        nrows=nrows,
+        ncols=ncols,
+        height=height,
+        width=width,
+        dpi=dpi,
     )
-    return parser.parse_args()
+    text = create_docs_text(transform_cls, args)
+
+    return text, image
+
+
+def create_and_save(
+    save_path,
+    transform_cls,
+    args,
+    image_path,
+    targets=None,
+    nrows=None,
+    ncols=None,
+    height=1080,
+    width=1920,
+    dpi=200,
+    show=True,
+):
+    text, image = create_example(
+        transform_cls, args, image_path, targets=targets, nrows=nrows, ncols=ncols, height=height, width=width, dpi=dpi
+    )
+
+    save_results(transform_cls, text, image, save_path)
+
+    if show:
+        show_image(image, dpi=dpi)
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    print(args)
-    # image = load_rgb_image('e:/Macaw Parrots image.jpg')
-    #
-    # args, image = apply_transform(
-    #     image,
-    #     A.Blur,
-    #     [
-    #         {'blur_limit': [7, 7]},
-    #         {'blur_limit': [15, 15]},
-    #         {'blur_limit': [30, 30]},
-    #         {'blur_limit': [60, 60]}
-    #     ]
-    # )
-    #
-    # cv2.imshow("", cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    # cv2.waitKey(0)
+    import albumentations as A
+
+    save_path = "../docs/augs_overview/image_only/image_only.rst"
+    image_path = "../notebooks/images/parrot.jpg"
+
+    transform = A.Blur
+    args = [{"blur_limit": [7, 7]}, {"blur_limit": [14, 14]}, {"blur_limit": [28, 28]}, {"blur_limit": [56, 56]}]
+
+    create_and_save(save_path, transform, args, image_path)
